ea5461a89ccc47825c665f934dfd4d166d1d5f62
Brayan Muniz <35124364+brayanMuniz@users.noreply.github.com>
2025-04-10 07:45:16 -0500

    added tries amount to game

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -3,31 +3,37 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    loop {
-        println!("Please input your guess.");
-        let mut guess = String::new();
-
+    // Guess a random number
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
+    let mut tries_left: u32 = 10;
+    while tries_left > 0 {
+        println!("Enter your guess!");
+        let mut user_input = String::new();
         io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+            .read_line(&mut user_input)
+            .expect("Could not read it :(");
 
-        let guess: u32 = match guess.trim().parse() {
+        // convert to int
+        let user_input: u32 = match user_input.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        println!("You guessed {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
+        match user_input.cmp(&secret_number) {
+            Ordering::Less => println!("Your number was too small"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You win!");
+                println!("You got it");
                 break;
             }
         }
+
+        tries_left -= 1;
+    }
+
+    if tries_left > 0 {
+        println!("You won with {tries_left} tries left");
+    } else {
+        println!("Sorry, you lost");
     }
 }
