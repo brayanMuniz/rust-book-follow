85148024e2e7b846c2ce7226cc3e7724974a77a6
Brayan Muniz <35124364+brayanMuniz@users.noreply.github.com>
2025-04-19 06:57:07 -0500

    added pig latin

--- projects/chapter_8_exercises/src/main.rs
+++ projects/chapter_8_exercises/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     // 1. Given a list of integers, use a vector and return the median
     // (when sorted, the value in the middle position) and mode
     // (the value that occurs most often; a hash map will be helpful here) of the list.
+
     let list = [420, 1, 2, 3, 4, 50, 69, 420];
     println!("list is {:?}", list);
 
@@ -18,8 +19,16 @@ fn main() {
     // 2. Convert strings to pig latin. The first consonant of each word is moved to the end of the
     // word and ay is added, so first becomes irst-fay. Words that start with a vowel have hay added
     // to the end instead (apple becomes apple-hay). Keep in mind the details about UTF-8 encoding!
+
     let test_string = "I like anime".to_string(); // I-hay ike-lay anime-hay
     let pig_correct = "I-hay ike-lay anime-hay".to_string();
+    let my_attempt = convert_to_pig_latin(&test_string);
+    println!("Pig attempt: {my_attempt}");
+    if pig_correct == my_attempt {
+        println!("Pig Correct !");
+    } else {
+        println!("Pig NOT correct :(");
+    }
 
     // 3. Using a hash map and vectors, create a text interface to allow a user to add employee names
     // to a department in a company; for example, “Add Sally to Engineering” or “Add Amir to Sales.”
@@ -27,17 +36,23 @@ fn main() {
     // by department, sorted alphabetically.
 }
 
-// TODO:
 fn convert_to_pig_latin(str: &str) -> String {
-    let mut result = String::new();
-    let vowels = ["a", "e", "i", "o", "u"];
+    let mut result_vector = vec![];
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
     for word in str.split_whitespace() {
         for c in word.chars() {
-            // if vowels.contains() {} // best way to do this?
+            if vowels.contains(&c) {
+                result_vector.push(format!("{word}-hay"));
+                break;
+            } else {
+                let skip_first = word.chars().skip(1).collect::<String>();
+                result_vector.push(format!("{skip_first}-{c}ay"));
+                break;
+            }
         }
     }
 
-    return result.to_string();
+    result_vector.join(" ").to_string()
 }
 
 fn get_median(arr: [i32; 8]) -> i32 {
